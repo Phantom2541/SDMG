6c82db144de0771586397cd423d099313eab1a11
magtalas555 <150007355+magtalas555@users.noreply.github.com>
2023-12-19 17:10:31 +0800

    my profile

--- client/src/components/profile/image.jsx
+++ client/src/components/profile/image.jsx
@@ -1,114 +1,78 @@
-import React, { useEffect, useState } from "react";
-import {
-  MDBCol,
-  MDBCard,
-  MDBView,
-  MDBCardBody,
-  MDBAvatar,
-  MDBProgress,
-} from "mdbreact";
-import { useDispatch, useSelector } from "react-redux";
-import { PresetImage } from "../../services/utilities";
-import { useToasts } from "react-toast-notifications";
+import React from "react";
+import { MDBIcon } from "mdbreact";
+// import { useDispatch, useSelector } from "react-redux";
+// import { PresetImage } from "../../services/utilities";
+// import { useToasts } from "react-toast-notifications";
+import "./profile.css";
 
 export default function ProfileImage() {
-  const [file, setFile] = useState(null),
-    { auth, progressBar, image } = useSelector(({ auth }) => auth),
-    dispatch = useDispatch(),
-    { addToast } = useToasts();
+  // const [file, setFile] = useState(null),
+  //   { auth, progressBar, image } = useSelector(({ auth }) => auth),
+  //   dispatch = useDispatch(),
+  //   { addToast } = useToasts();
 
-  useEffect(() => {
-    if (file && progressBar === 100) {
-      // dispatch(IMAGE(URL.createObjectURL(file)));
-      setFile(null);
-      addToast("Image Updated Successfully.", {
-        appearance: "success",
-      });
-    }
-  }, [progressBar, file, dispatch, addToast]);
+  // useEffect(() => {
+  //   if (file && progressBar === 100) {
+  // dispatch(IMAGE(URL.createObjectURL(file)));
+  //     setFile(null);
+  //     addToast("Image Updated Successfully.", {
+  //       appearance: "success",
+  //     });
+  //   }
+  // }, [progressBar, file, dispatch, addToast]);
 
-  const handleError = (message) =>
-    addToast(message, {
-      appearance: "warning",
-    });
+  // const handleError = (message) =>
+  //   addToast(message, {
+  //     appearance: "warning",
+  //   });
 
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
+  // const handleImageChange = (e) => {
+  //   const file = e.target.files[0];
 
-    if (file.type !== "image/jpeg" || file.type !== "image/jpg")
-      return handleError("Please select a JPG image.");
+  //   if (file.type !== "image/jpeg" || file.type !== "image/jpg")
+  //     return handleError("Please select a JPG image.");
 
-    const reader = new FileReader();
+  //   const reader = new FileReader();
 
-    reader.onload = (e) => {
-      const img = new Image();
-      img.src = e.target.result;
+  //   reader.onload = (e) => {
+  //     const img = new Image();
+  //     img.src = e.target.result;
 
-      img.onload = function () {
-        if (this.width !== this.height)
-          return handleError("Image must be square.");
+  //     img.onload = function () {
+  //       if (this.width !== this.height)
+  //         return handleError("Image must be square.");
 
-        if (this.width > 100)
-          return handleError("Image too big, maximum size is 100 pixels.");
+  //       if (this.width > 100)
+  //         return handleError("Image too big, maximum size is 100 pixels.");
 
-        setFile(file);
-        dispatch();
-        // UPLOAD({
-        //   data: {
-        //     path: `${auth.email}`,
-        //     base64: reader.result.split(",")[1],
-        //     name: "profile.jpg",
-        //   },
-        //   token,
-        // })
-      };
-    };
+  //       setFile(file);
+  //       dispatch();
+  // UPLOAD({
+  //   data: {
+  //     path: `${auth.email}`,
+  //     base64: reader.result.split(",")[1],
+  //     name: "profile.jpg",
+  //   },
+  //   token,
+  // })
+  //     };
+  //   };
 
-    reader.readAsDataURL(file);
-  };
+  //   reader.readAsDataURL(file);
+  // };
 
   return (
-    <MDBCol lg="3" className="mb-4">
-      <MDBCard narrow>
-        <MDBView cascade className="mdb-color lighten-3 card-header">
-          <h5 className="mb-0 font-weight-bold text-center text-white">
-            Edit Photo
-          </h5>
-        </MDBView>
-        <MDBCardBody className="text-center">
-          {auth._id && (
-            <MDBAvatar
-              tag="img"
-              src={image}
-              onError={(e) => (e.target.src = PresetImage(auth.isMale))}
-              alt={`preview-${auth._id}`}
-              className="z-depth-1 mb-3 mx-auto rounded"
-            />
-          )}
-
-          {progressBar >= 0 && <MDBProgress value={progressBar} animated />}
-          <p className="text-muted">
-            <small>
-              {progressBar > -1
-                ? "Please wait while we update your profile photo"
-                : "Profile photo will be changed automatically"}
-            </small>
-          </p>
-          <label
-            htmlFor="changeImage"
-            className="btn btn-info btn-sm btn-rounded"
-          >
-            Upload New Photo
-          </label>
-          <input
-            id="changeImage"
-            onChange={handleImageChange}
-            type="file"
-            className="d-none"
-            accept="image/jpeg, image/jpg"
-          />
-        </MDBCardBody>
-      </MDBCard>
-    </MDBCol>
+    <>
+      <div className="profile-container">
+        <img
+          src="https://scontent.fcrk2-2.fna.fbcdn.net/v/t1.18169-9/13177391_952786094834165_1174862213095433470_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=7a1959&_nc_eui2=AeE7DE7_uzpgvXpdJKXZzWNugaoQP3N0HdCBqhA_c3Qd0HSialDzbhbbR6GQQ0Sw8pRV7YmK8ldIAVFwZO2LbmKQ&_nc_ohc=0YSkcYwP0IEAX-0p8RD&_nc_ht=scontent.fcrk2-2.fna&oh=00_AfBpxx8uCEh3xMqNji3oc500uvUXmPxfYV1suaFJ57FPyg&oe=65A8A2F4"
+          className="school-profile"
+          alt="profile"
+        />
+        <label className="profile-camera">
+          <MDBIcon icon="camera" />
+        </label>
+      </div>
+    </>
   );
 }
